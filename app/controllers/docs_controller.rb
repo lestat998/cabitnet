52c78277c58361c8ade047fff5a9f99aa2906369
Rockero <rockero@localhost.localdomain>
2017-07-08 15:29:12 -0400

    Add docs views, define actions and change erb to haml

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,15 +1,26 @@
 class DocsController < ApplicationController
+	before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@docs = Doc.all.order("create_at DESC")
 	end
 
 	def show
+
 	end
 
 	def new
+		@doc = Doc.new
 	end
 
 	def create
+		@doc = Doc.new(doc_params)
+
+		if @doc.save
+			redirect_to @doc
+		else
+			render 'new'
+		end
 	end
 
 	def edit
@@ -18,14 +29,16 @@ class DocsController < ApplicationController
 	def update
 	end
 
-	def destroy
+	def destroy 
 	end
 
 	private
 
 		def find_doc
+			@doc = Doc.find(params[:id])
 		end
 
 		def doc_params
-		end
+			params.require(:doc).permit(:title, :content )
+		end 
 end
